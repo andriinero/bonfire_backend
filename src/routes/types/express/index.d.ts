9c9e05e7cf0387f8d79b48f136d3c6a80e8bc5fc
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-05-31 13:57:54 +0200

    fix: user type declaration

--- src/routes/types/express/index.d.ts
+++ src/routes/types/express/index.d.ts
@@ -1,11 +1,16 @@
+import { TUserDocument } from '@src/models/User';
 import 'express';
-
 
 // **** Declaration Merging **** //
 
 declare module 'express' {
-
   export interface Request {
     signedCookies: Record<string, string>;
+  }
+}
+
+declare global {
+  namespace Express {
+    export interface User extends TUserDocument {}
   }
 }
