2cc3482fe08e1a2b26a5dfb60d1ab1755b7cd0e0
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2025-01-03 12:58:25 +0100

    fix: error text typo

--- src/other/errors/NotFoundError.ts
+++ src/other/errors/NotFoundError.ts
@@ -1,7 +1,7 @@
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
 import { RouteError } from '../classes';
 
-const NOT_FOUND = 'Object not found ';
+const NOT_FOUND = 'Object not found';
 
 export default class NotFoundError extends RouteError {
   public constructor(message = NOT_FOUND) {
